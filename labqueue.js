240281197f095c5f89c3f6b22f050d8f2d328167
Linus Karlsson <linus@zozs.se>
2015-06-16 17:00:40 +0200

    Add some unit tests using Mocha.

--- labqueue.js
+++ labqueue.js
@@ -4,9 +4,25 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var config = require('./config');
 
+// For automated testing purposes, we may start the server with a special flag.
+// Do NOT use this flag during normal operation!
+var testMode = false;
+
+if (process.argv.length == 3 && process.argv[2] == '--testing') {
+  testMode = true;
+  // Load special config for testing.
+  config = require('./config-test');
+  console.log('WARNING! Test mode activated. Do NOT use in production!');
+}
+
 // Prepare database.
+var databaseFile = 'queue.sqlite';
+if (testMode) {
+  databaseFile = ':memory:';
+}
+
 var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('queue.sqlite');
+var db = new sqlite3.Database(databaseFile);
 
 db.run('CREATE TABLE IF NOT EXISTS queue (' +
        'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
@@ -18,12 +34,28 @@ db.run('CREATE TABLE IF NOT EXISTS queue (' +
 // Serve static files.
 app.use(express.static(__dirname + '/public'));
 
+// Testing stuff, allows the client to manually select IP during testing.
+if (testMode) {
+  io.use(function(socket, next) {
+    var mockIp = socket.request._query['mockIp'];
+    if (mockIp) {
+      socket.labqueue = { mockIp: mockIp };
+      console.log("Mocking IP to: ", socket.labqueue.mockIp);
+    }
+    next();
+  });
+}
+
 // Listen for socket.io stuff.
 io.on('connection', function(socket) {
   var remote_ip = socket.request.connection.remoteAddress;
+  if (testMode && 'labqueue' in socket && 'mockIp' in socket.labqueue) {
+    remote_ip = socket.labqueue.mockIp;
+  }
+
   var subject = ip_to_subject(remote_ip);
   var is_admin = (config.admins.indexOf(remote_ip) > -1);
-  console.log(subject + ' connected. Administrator?: ' + is_admin);
+  //console.log(subject + ' connected. Administrator?: ' + is_admin);
 
   // First send the client name to the client.
   socket.emit('clientname', subject);
@@ -45,7 +77,7 @@ io.on('connection', function(socket) {
         if (err.errno == 19) { /* SQLITE_CONSTRAINT */
           socket.emit('queueFail', 'You already have a help request!');
         } else {
-          console.log(JSON.stringify(err));
+          console.log('Unhandled help me error: ', JSON.stringify(err));
           socket.emit('queueFail', 'Failed to ask for help...');
         }
       } else {
@@ -111,14 +143,25 @@ io.on('connection', function(socket) {
     socket.on('undelete', err_func);
     socket.on('delete', err_func);
   }
+
+  if (testMode) {
+    socket.on('reset', function() {
+      db.run('DELETE FROM queue;', function(err) {
+        check_error(err, socket, function() {
+          socket.emit('resetted');
+        });
+      });
+    });
+  }
 });
 
 http.listen(3000, '0.0.0.0');
+exports.http = http
 
 function check_error(err, socket, success_handler) {
   if (err) {
     socket.emit('queueFail', 'Database error!');
-    console.log(err);
+    console.log('Unhandled database error: ', err);
   } else {
     success_handler();
   }
@@ -128,9 +171,16 @@ function delete_top(socket, send_queue_func) {
   var sql = 'UPDATE queue SET done=CURRENT_TIMESTAMP WHERE id IN ' +
             '(SELECT id FROM queue WHERE done=0 ORDER BY added, id LIMIT 1);';
   db.run(sql, function(err) {
-    check_error(err, socket, function() {
-      send_queue_func();
-    });
+    if (err && err.errno == 19) {
+      /* Can occur if the students spam the help me/never mind button so that
+       * they trigger the UNIQUE-constraint of (subject, done). Tell them to
+       * calm the fuck down, or just ignore it. */
+      //socket.emit('queueFail', "Srsly, stop spamming the buttons :'(");
+    } else {
+      check_error(err, socket, function() {
+        send_queue_func();
+      });
+    }
   });
 }
 
---
+++ test/test-labqueue.js
@@ -0,0 +1,228 @@
+var should = require('should');
+var io = require('socket.io-client');
+
+var socketURL = 'http://localhost:3000';
+
+var client1details = { ip: '192.0.2.1', name: 'C1' };
+var options1 = {
+  'forceNew': true,
+  query: 'mockIp=' + client1details.ip // TEST-NET
+};
+
+var client2details = { ip: '192.0.2.2', name: 'C2' };
+var options2 = {
+  'forceNew': true,
+  query: 'mockIp=' + client2details.ip // TEST-NET
+};
+
+var client3details = { ip: '192.0.2.3', name: 'C3' };
+var options3 = {
+  'forceNew': true,
+  query: 'mockIp=' + client3details.ip // TEST-NET
+};
+
+var clients = [client1details, client2details, client3details];
+
+describe("Labqueue", function() {
+  var client1, client2;
+
+  beforeEach(function(done) {
+    client1 = io.connect(socketURL, options1);
+
+    client1.once('connect', function() {
+      client1.emit('reset');
+      client1.once('resetted', function() {
+        client2 = io.connect(socketURL, options2);
+        client2.once('connect', function() {
+          client2.emit('reset');
+          client2.once('resetted', function() {
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    client2.disconnect();
+    client1.emit('reset');
+    client1.once('resetted', function() {
+      client1.disconnect();
+      done();
+    });
+  });
+
+  it('should connect', function(done) {
+    var client3 = io.connect(socketURL, options3);
+    client3.once('connect', function (msg) {
+      client3.disconnect();
+      done();
+    });
+  });
+
+  it('should receive client name on start', function(done) {
+    var client3 = io.connect(socketURL, options3);
+    client3.once('clientname', function (msg) {
+      client3.disconnect();
+      msg.should.equal(client3details.name);
+      done();
+    });
+  });
+
+  it('should receive empty queue on start', function(done) {
+    var client3 = io.connect(socketURL, options3);
+    client3.once('queue', function (msg) {
+      client3.disconnect();
+      done();
+    });
+  });
+
+  it('should be able to see yourself', function(done) {
+    client1.emit('helpme');
+    client1.once('queue', function() {
+      checkQueue([1], done);
+    });
+  });
+
+  it('should be able to see a bunch of people', function(done) {
+    client1.emit('helpme');
+    client1.once('queue', function() {
+      client2.emit('helpme');
+      client2.once('queue', function() {
+        checkQueue([1, 2], done);
+      });
+    });
+  });
+
+  it('should be able to delete myself', function(done) {
+    client1.emit('helpme');
+    client1.once('queue', function() {
+      client1.emit('nevermind');
+      client1.once('queue', function() {
+        checkQueue([], done);
+      });
+    });
+  });
+
+  it('should not be able to add myself twice', function(done) {
+    client1.emit('helpme');
+    client1.once('queue', function() {
+      client1.emit('helpme');
+      client1.once('queueFail', function() {
+        done();
+      });
+    });
+  });
+  
+  it('admin should be able to delete top', function(done) {
+    client2.emit('helpme');
+    client2.once('queue', function() {
+      client1.emit('delete');
+      client1.once('queue', function() {
+        checkQueue([], done);
+      });
+    });
+  });
+  
+  it('should not be able to delete me if not in queue', function(done) {
+    client2.emit('helpme');
+    client2.once('queue', function() {
+      client1.emit('nevermind');
+      client1.once('queueFail', function() {
+        checkQueue([2], done);
+      });
+    });
+  });
+
+  it('should not be able to delete top if not admin', function(done) {
+    client1.emit('helpme');
+    client1.once('queue', function() {
+      client2.emit('delete');
+      client2.once('queueFail', function() {
+        checkQueue([1], done);
+      });
+    });
+  });
+
+  it('should be able to undelete some', function(done) {
+    client1.emit('helpme');
+    client1.once('queue', function() {
+      client2.emit('helpme');
+      client2.once('queue', function() {
+        client1.emit('delete');
+        client1.once('queue', function() {
+          client1.emit('delete');
+          client1.once('queue', function() {
+            client1.emit('undelete');
+            client1.once('queue', function() {
+              checkQueue([2], function() {
+                client1.emit('undelete');
+                client1.once('queue', function() {
+                  checkQueue([1, 2], done);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+  
+  it('should not be able to undelete if not admin', function(done) {
+    client1.emit('helpme');
+    client1.once('queue', function() {
+      client1.emit('delete');
+      client1.once('queue', function() {
+        client2.emit('undelete');
+        client2.once('queueFail', function() {
+        checkQueue([], done);
+        });
+      });
+    });
+  });
+
+
+  it('should not be able to undelete and give duplicates', function(done) {
+    /* Ugly test since we somehow need to get different timestamps on the 
+     * deleted entries to avoid the uniqueness-constraint to be violated. */
+    client2.emit('helpme');
+    client2.once('queue', function(q) {
+      client1.emit('delete');
+      client1.once('queue', function(q) {
+        setTimeout(function() { // <- delay start.
+          client2.emit('helpme');
+          client2.once('queue', function(q) {
+            client1.emit('delete');
+            client1.once('queue', function(q) {
+              checkQueue([], function() {
+                client1.emit('undelete');
+                client1.once('queue', function() {
+                  checkQueue([2], function() {
+                    client1.emit('undelete');
+                    client1.once('queueFail', function() {
+                      checkQueue([2], done);
+                    });
+                  });
+                });
+              });
+            });
+          });
+        }, 1000); // <- delay end.
+      });
+    });
+  });
+});
+
+/* Call like this: checkQueue([1, 3, 2], done). Opens a new connection to get
+ * the current queue. */
+function checkQueue(expected, done) {
+  var checkClient = io.connect(socketURL, options3);
+  checkClient.once('queue', function(q) {
+    q.queue.length.should.equal(expected.length);
+    for (var i = 0; i < expected.length; i++) {
+      q.queue[i].subject.should.equal(clients[expected[i] - 1].name);
+    }
+    checkClient.disconnect();
+    done();
+  });
+}
