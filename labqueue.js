0ccc75b6cb084a0766d941eca2af9896351f5fbd
Linus Karlsson <linus@zozs.se>
2015-07-03 13:04:07 +0200

    Add whitelist functionality to limit queue access

--- labqueue.js
+++ labqueue.js
@@ -61,7 +61,8 @@ io.on('connection', function (socket) {
 
   var subject = ip_to_subject(remote_ip);
   var is_admin = (config.get('admins').indexOf(remote_ip) > -1);
-  //console.log(subject + ' connected. Administrator?: ' + is_admin);
+  var whitelist = config.get('whitelist');
+  var is_whitelisted = (whitelist.length === 0 || whitelist.indexOf(remote_ip) > -1);
 
   // First send the client name to the client.
   socket.emit('clientname', subject);
@@ -77,79 +78,91 @@ io.on('connection', function (socket) {
   };
   send_queue(socket);
 
-  socket.on('helpme', function () {
-    db.run('INSERT INTO queue (subject) VALUES (?);', subject, function (err) {
-      if (err) {
-        if (err.errno === 19) { /* SQLITE_CONSTRAINT */
-          socket.emit('queueFail', 'You already have a help request!');
-        } else {
-          console.log('Unhandled help me error: ', JSON.stringify(err));
-          socket.emit('queueFail', 'Failed to ask for help...');
-        }
-      } else {
-        // Send queue to everyone.
-        send_queue(io);
-      }
-    });
-  });
-
-  socket.on('nevermind', function () {
-    var sql = 'UPDATE queue SET done=CURRENT_TIMESTAMP,removedSelf=1 WHERE ' +
-              'subject=? AND done=0;';
-    db.run(sql, subject, function (err) {
-      var changes = this.changes;
-      if (err && err.errno === 19) {
-        /* Can occur if the students spam the help me/never mind button so that
-         * they trigger the UNIQUE-constraint of (subject, done). Tell them to
-         * calm the fuck down, delete the duplicate row, or just ignore it. */
-        //socket.emit('queueFail', "Srsly, stop spamming the buttons :'(");
-      } else {
-        check_error(err, socket, function () {
-          if (changes === 0) {
-            socket.emit('queueFail', 'You have no help request to delete!');
+  if (is_whitelisted) {
+    socket.on('helpme', function () {
+      db.run('INSERT INTO queue (subject) VALUES (?);', subject, function (err) {
+        if (err) {
+          if (err.errno === 19) { /* SQLITE_CONSTRAINT */
+            socket.emit('queueFail', 'You already have a help request!');
           } else {
-            send_queue(io);
+            console.log('Unhandled help me error: ', JSON.stringify(err));
+            socket.emit('queueFail', 'Failed to ask for help...');
+          }
+        } else {
+          // Send queue to everyone.
+          send_queue(io);
+        }
+      });
+    });
+
+    socket.on('nevermind', function () {
+      var sql = 'UPDATE queue SET done=CURRENT_TIMESTAMP,removedSelf=1 WHERE ' +
+                'subject=? AND done=0;';
+      db.run(sql, subject, function (err) {
+        var changes = this.changes;
+        if (err && err.errno === 19) {
+          /* Can occur if the students spam the help me/never mind button so that
+           * they trigger the UNIQUE-constraint of (subject, done). Tell them to
+           * calm the fuck down, delete the duplicate row, or just ignore it. */
+          //socket.emit('queueFail', "Srsly, stop spamming the buttons :'(");
+        } else {
+          check_error(err, socket, function () {
+            if (changes === 0) {
+              socket.emit('queueFail', 'You have no help request to delete!');
+            } else {
+              send_queue(io);
+            }
+          });
+        }
+      });
+    });
+
+    if (is_admin) {
+      socket.on('undelete', function () {
+        var sql = 'UPDATE queue SET done=0,removedSelf=NULL WHERE id IN ' +
+                  '(SELECT id FROM queue WHERE done!=0 ORDER BY added DESC, id DESC LIMIT 1);';
+        db.run(sql, function (err) {
+          if (err && err.errno === 19) {
+            socket.emit('queueFail', 'Undo would result in duplicate help requests for user.');
+          } else {
+            check_error(err, socket, function () {
+              send_queue(io);
+            });
           }
         });
-      }
-    });
-  });
-
-  if (is_admin) {
-    socket.on('undelete', function () {
-      var sql = 'UPDATE queue SET done=0,removedSelf=NULL WHERE id IN ' +
-                '(SELECT id FROM queue WHERE done!=0 ORDER BY added DESC, id DESC LIMIT 1);';
-      db.run(sql, function (err) {
-        if (err && err.errno === 19) {
-          socket.emit('queueFail', 'Undo would result in duplicate help requests for user.');
-        } else {
-          check_error(err, socket, function () {
-            send_queue(io);
-          });
-        }
       });
-    });
 
-    socket.on('delete', function () {
-      var sql = 'UPDATE queue SET done=CURRENT_TIMESTAMP,removedSelf=0 WHERE id IN ' +
-                '(SELECT id FROM queue WHERE done=0 ORDER BY added, id LIMIT 1);';
-      db.run(sql, function (err) {
-        if (err && err.errno === 19) {
-          /* Can occur if the same subjects gets delete twice during the same
-           * second by the teacher. Just ignore if this happened. */
-        } else {
-          check_error(err, socket, function () {
-            send_queue(io);
-          });
-        }
+      socket.on('delete', function () {
+        var sql = 'UPDATE queue SET done=CURRENT_TIMESTAMP,removedSelf=0 WHERE id IN ' +
+                  '(SELECT id FROM queue WHERE done=0 ORDER BY added, id LIMIT 1);';
+        db.run(sql, function (err) {
+          if (err && err.errno === 19) {
+            /* Can occur if the same subjects gets delete twice during the same
+             * second by the teacher. Just ignore if this happened. */
+          } else {
+            check_error(err, socket, function () {
+              send_queue(io);
+            });
+          }
+        });
       });
-    });
+    } else {
+      /* If not admin */
+      var err_func = function () {
+        socket.emit('queueFail', 'You are not an administrator!');
+      };
+      socket.on('undelete', err_func);
+      socket.on('delete', err_func);
+    }
   } else {
-    var err_func = function () {
-      socket.emit('queueFail', 'You are not an administrator!');
+    /* If not whitelisted. */
+    var not_whitelisted_func = function () {
+      socket.emit('queueFail', 'You are not allowed in this queue!');
     };
-    socket.on('undelete', err_func);
-    socket.on('delete', err_func);
+    socket.on('undelete', not_whitelisted_func);
+    socket.on('delete', not_whitelisted_func);
+    socket.on('nevermind', not_whitelisted_func);
+    socket.on('helpme', not_whitelisted_func);
   }
 
   if (testMode) {
